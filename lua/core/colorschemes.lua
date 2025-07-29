f9d5198104c34b97a9c265d9508c13e21e228819
Ruben Hesselink <ruben@fourdigits.nl>
2025-07-29 14:40:14 +0200

    Add some more colorschemes

--- lua/core/colorschemes.lua
+++ lua/core/colorschemes.lua
@@ -3,6 +3,11 @@ local add, now = MiniDeps.add, MiniDeps.now
 now(function()
   add({ source = 'vague2k/vague.nvim' })
   add({ source = 'rose-pine/neovim' })
+  add({ source = 'folke/tokyonight.nvim' })
+  add({ source = 'catppuccin/nvim' })
+  add({ source = 'rebelot/kanagawa.nvim' })
+  add({ source = 'olimorris/onedarkpro.nvim' })
+  add({ source = 'loctvl842/monokai-pro.nvim' })
 
-  vim.cmd('colorscheme vague')
+  vim.cmd('colorscheme catppuccin-latte')
 end)
